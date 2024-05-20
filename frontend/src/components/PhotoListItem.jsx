49fad109e694903a1b8b16999dfab8eadb6dc492
Tommy <tommy.azran@gmail.com>
2024-05-20 18:44:24 -0400

    initial styling to photo list item

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,11 +5,13 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({photo: {imageSource, profile, username, location: {city, country}}}) => {
   /* Insert React */
   return (
-    <div className="photo-list-item">
-      <img className="image-source" src={imageSource}/>
-      <img className="profile" src={profile}/>
-      <p className="username">{username}</p>
-      <p className="location">{city}, {country}</p>
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={imageSource}/>
+      <img className="photo-list__user-profile" src={profile}/>
+      <div className="photo-list__user-details">
+        <p className="photo-list__user-info">{username}</p>
+        <p className="photo-list__user-location">{city}, {country}</p>
+      </div>
     </div>
   );
 };
