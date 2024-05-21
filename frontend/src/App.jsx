a89ab718de2f48febc130ada63c02f795a9e2109
Tommy <tommy.azran@gmail.com>
2024-05-21 11:33:06 -0400

    photolist.jsx render hard code dynamically

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,47 +3,16 @@ import PhotoListItem from 'components/PhotoListItem';
 import PhotoList from './components/PhotoList';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  "1": {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada"
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`
-  },
-  "2": {
-    id: "2",
-    location: {
-      city: "Montreal",
-      country: "Canada"
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`
-  },
-  "3": {
-    id: "3",
-    location: {
-      city: "Montreal",
-      country: "Canada"
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`
-  }
-};
 
 // Note: Rendering a single component to build components in isolation
 const App = (props) => {
-  const photos = Object.values(sampleDataForPhotoListItem);
+  // const photos = Object.values(sampleDataForPhotoListItem);
   return (
     <div className="App">
-      {photos.map(photo =>
+      {/* {photos.map(photo =>
         <PhotoListItem key={photo.id} photo={photo} />
-      )}
+      )} */}
+      <PhotoList />
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,15 +3,15 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
-const PhotoListItem = ({photo: {imageSource, profile, username, location: {city, country}}}) => {
+const PhotoListItem = ({photo: { urls:{full}, user:{name, profile}, location: {city, country}}}) => {
   /* Insert React */
   return (
     <div className="photo-list__item">
       <PhotoFavButton />
-      <img className="photo-list__image" src={imageSource}/>
+      <img className="photo-list__image" src={full}/>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={profile}/>
-        <p className="photo-list__user-info">{username}</p>
+        <p className="photo-list__user-info">{name}</p>
         <p className="photo-list__user-location">{city}, {country}</p>
       </div>
     </div>
