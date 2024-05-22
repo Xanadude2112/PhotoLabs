ea58458617b028ae717da368e31ad9173c4d3e34
Tommy <tommy.azran@gmail.com>
2024-05-22 12:49:46 -0400

    change styling modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -34,25 +34,19 @@ const PhotoDetailsModal = ({
       <button className="photo-details-modal__close-button" onClick={showModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      <img className="photo-details-modal__image" src={full} />
-      <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={profile} />
-        <p className="photo-list__user-info">{name}</p>
-        <p className="photo-list__user-location">
-          {city}, {country}
-        </p>
+      <div className="modal_content">
+        <PhotoFavButton fave={fave} setFave={setFave} id={id} />
+        <img className="photo-details-modal__image" src={full} />
+        <div className="photo-list__user-details">
+          <img className="photo-list__user-profile" src={profile} />
+          <p className="photo-list__user-info">{name}</p>
+          <p className="photo-list__user-location">
+            {city}, {country}
+          </p>
+        </div>
       </div>
       <h3>Similar Photos</h3>
-      <PhotoList
-        photos={photos}
-        fave={fave}
-        setFave={setFave}
-        modalOpen={modalOpen}
-        setModalOpen={setModalOpen}
-        showModal={showModal}
-        selectedPhoto={selectedPhoto}
-        setSelectedPhoto={setSelectedPhoto}
-      />
+      <PhotoList photos={photos} fave={fave} setFave={setFave} />
     </div>
   );
 };
